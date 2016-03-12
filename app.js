e065eb2c24d859f5b1e819350a5b5b974d6aa61a
mbthoms <mbthoms@outlook.com>
2016-03-11 22:26:58 -0500

    Routing all pages.
    Adding all pages. Working on nabber.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var landing = require('./routes/landing');
+var map = require('./routes/map');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/landing', landing);
+app.use('/map', map);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/landing.js
+++ routes/landing.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET landing page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Login Form - Police Studies' });
+    res.render('landing', { title: 'Landing - Police Studies' });
 });
 
 module.exports = router;
